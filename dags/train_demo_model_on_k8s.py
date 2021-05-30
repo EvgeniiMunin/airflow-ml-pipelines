b4b01458cae812560b930c9d7edbf6cd3f18361e
Mikhail-M <mihail-maryfich@yandex.ru>
2021-05-30 21:32:50 +0300

    add secrets

--- dags/train_demo_model_on_k8s.py
+++ dags/train_demo_model_on_k8s.py
@@ -1,4 +1,5 @@
 from airflow import models
+from airflow.kubernetes.secret import Secret
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
     KubernetesPodOperator,
 )
@@ -10,6 +11,30 @@ with models.DAG(
     start_date=days_ago(1),
     tags=["example"],
 ) as dag:
+    secret_aws_key_id = Secret(
+        # Expose the secret as environment variable.
+        deploy_type="env",
+        # The name of the environment variable, since deploy_type is `env` rather
+        # than `volume`.
+        deploy_target="AWS_ACCESS_KEY_ID",
+        # Name of the Kubernetes Secret
+        secret="aws-s3-secret",
+        # Key of a secret stored in this Secret object
+        key="AWS_ACCESS_KEY_ID",
+    )
+
+    secret_aws_access_key = Secret(
+        # Expose the secret as environment variable.
+        deploy_type="env",
+        # The name of the environment variable, since deploy_type is `env` rather
+        # than `volume`.
+        deploy_target="AWS_SECRET_ACCESS_KEY",
+        # Name of the Kubernetes Secret
+        secret="aws-s3-secret",
+        # Key of a secret stored in this Secret object
+        key="AWS_SECRET_ACCESS_KEY",
+    )
+
     train_model = KubernetesPodOperator(
         task_id="train-heart-ml-model",
         name="train-heart-ml-model",
@@ -21,6 +46,7 @@ with models.DAG(
             "--s3_bucket",
             "demo-cicd-made",
         ],
+        secrets=[secret_aws_key_id, secret_aws_access_key],
         namespace="airflow-stage",
         service_account_name="airflow-scheduler",
         image="mikhailmar/training-job:pr-1",
